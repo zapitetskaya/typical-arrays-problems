5c4b3044955328341cbe98cd26ad2c82916994ea
Запитецкая Алена Евгеньевна <190032@edu.psuti.ru>
2021-03-08 22:05:49 +0400

    feat: add function: min,max,avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+    //if (array.isEmpty()){
+        let min = array.length;
+        for (let i = 0; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+        return min;
+    //} else {
+    //    return 0;
+    //}
 }
 
 exports.max = function max (array) {
-  return 0;
+    //if (array.isEmpty()){
+    let max = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (max < array[i]) {
+            max = array[i];
+        }
+    }
+    return max;
+    //}
+    //else {
+    //     return 0;
+    //}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    //if (array.isEmpty()){
+    let avg = 0;
+    for (let i = 0; i < array.length; i++) {
+        avg += array[i];
+    }
+    avg /= array.length;
+    return avg;
+    //}
+    //else {
+    //    return 0;
+    //}
 }
+
